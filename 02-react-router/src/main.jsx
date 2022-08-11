fcfe200e7f99a69f039f69cfcbb7a399122e7c03
César Guerra <warderer@gmail.com>
2022-08-10 19:39:47 -0500

    ✨ Ruteo básico

--- 02-react-router/src/main.jsx
+++ 02-react-router/src/main.jsx
@@ -2,9 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+/* Importar BrowserRouter, que dota a mi App de poder manejar rutas
+Debe estar en un componente superior, para que este disponible en toda
+la aplicación */
+import { BrowserRouter as Router } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </React.StrictMode>
 )
