01e5bf5eb77bcfb6d3164ed44e925fb6ec71792a
César Guerra <warderer@gmail.com>
2022-08-15 20:39:27 -0500

    ✨ Página Home básica

---
+++ 03-react-context/src/pages/Home/Home.jsx
@@ -0,0 +1,17 @@
+import './home.css'
+
+const Home = () => {
+  return (
+    <div className='home-container'>
+      <div className='izquierdo'>
+        <h2>Lado Izquierdo</h2>
+      </div>
+
+      <div className='derecho'>
+        <h2>Lado Derecho</h2>
+      </div>
+    </div>
+  )
+}
+
+export default Home
