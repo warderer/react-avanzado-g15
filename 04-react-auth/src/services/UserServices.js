6bace1b6980b90872668a7ee3633c80bbb39e542
César Guerra <warderer@gmail.com>
2022-08-27 20:22:32 -0500

    ✨ Se añade Service getSingleUser

--- 04-react-auth/src/services/UserServices.js
+++ 04-react-auth/src/services/UserServices.js
@@ -1,7 +1,13 @@
-import axios from 'axios';
-const BASE_URL = 'https://ecomerce-master.herokuapp.com/api/v1';
+import axios from 'axios'
+const BASE_URL = 'https://ecomerce-master.herokuapp.com/api/v1'
 
-const RegisterUser = (data) => axios.post(`${BASE_URL}/signup`, data);
-const LoginUser = (data) => axios.post(`${BASE_URL}/login`, data);
+const token = window.sessionStorage.getItem('token') || ''
+const config = {
+  headers: { Authorization: `JWT ${token}` }
+}
 
-export { RegisterUser, LoginUser };
+const RegisterUser = (data) => axios.post(`${BASE_URL}/signup`, data)
+const LoginUser = (data) => axios.post(`${BASE_URL}/login`, data)
+const getSingleUser = (id) => axios.get(`${BASE_URL}/user/${id}`, config)
+
+export { RegisterUser, LoginUser, getSingleUser }
